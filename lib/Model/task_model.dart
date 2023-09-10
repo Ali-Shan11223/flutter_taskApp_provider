343631105da150763689f26652fefcdaada830d2
Ali-Shan11223 <jonah2114@gmail.com>
2023-09-10 22:46:10 +0500

    Model

---
+++ lib/Model/task_model.dart
@@ -0,0 +1,15 @@
+class Task {
+  int? id;
+  String title;
+  String description;
+
+  Task({this.id, required this.title, required this.description});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'title': title, 'description': description};
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(title: map['title'], description: map['description']);
+  }
+}
---
+++ lib/Views/task_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TaskScreen extends StatefulWidget {
+  const TaskScreen({super.key});
+
+  @override
+  State<TaskScreen> createState() => _TaskScreenState();
+}
+
+class _TaskScreenState extends State<TaskScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tasks'),
+      ),
+    );
+  }
+}
